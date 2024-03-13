4d35dde57f7639791a9cc4ddc60611d7b7c6d938
clydekuo <clydekuo6@gmail.com>
2024-03-13 00:48:15 -0700

    feat: initial

---
+++ initialize-genesis.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+set -eu
+
+# rm -rf $(pwd)/validator_keys
+# docker run -it --rm -v $(pwd)/validator_keys:/app/validator_keys clydekuo/staking-deposit-cli  --language=English  new-mnemonic --num_validators=2   --mnemonic_language=english  --chain=testnet 
+
+
+# Creates a genesis state for the beacon chain using a YAML configuration file 
+deposit_data=$(ls validator_keys/deposit_data-*.json)
+docker run --rm \
+  -v "$(pwd)/config:/config" \
+  -v "$(pwd)/execution:/execution" \
+  -v "$(pwd)/consensus:/consensus" \
+  -v "$(pwd)/validator_keys:/validator_keys" \
+  gcr.io/prysmaticlabs/prysm/cmd/prysmctl \
+  testnet \
+  generate-genesis \
+  --num-validators 2 \
+  --chain-config-file /config/beacon-chain-config.yml \
+  --deposit-json-file /$deposit_data \
+  --geth-genesis-json-in /config/genesis.json \
+  --geth-genesis-json-out /config/genesis.json \
+  --output-ssz /consensus/genesis.ssz
+
+rm -rf $(pwd)/execution/
+docker run --rm \
+  -v "$(pwd)/execution":/execution \
+  gcr.io/prysmaticlabs/prysm/beacon-chain:v4.1.1 \
+  generate-auth-secret \
+  -o /execution/jwtsecret
+
+# Sets up the genesis configuration for the go-ethereum client from a JSON file.
+docker run --rm \
+  -v "$(pwd)/execution:/execution" \
+  -v "$(pwd)/config:/config" \
+  ethereum/client-go:latest \
+  init \
+  --datadir=/execution \
+  --state.scheme=hash \
+  /config/genesis.json
+
+
+docker run --rm --name validator -it --network=devnet \
+  -v "$(pwd)/consensus:/consensus" \
+  -v "$(pwd)/validator_keys:/validator_keys" \
+  gcr.io/prysmaticlabs/prysm/validator:v4.1.1 \
+  accounts \
+  import \
+  --keys-dir=/validator_keys \
+  --wallet-dir=/consensus/prysmwallet \
+  --accept-terms-of-use
